0220abf69986b167658fae2595fb67c2f79b4ee0
hantabaru1014 <hantabaru1014@gmail.com>
2020-06-17 00:00:34 +0900

    マシュマロビューで伏せ字マシュマロが表示されない等の不具合を修正

--- mmview_inject.js
+++ mmview_inject.js
@@ -7,9 +7,9 @@ window.addEventListener('load', () => {
         $('body').css({'background-color': arg});
     });
     ipcRenderer.on('changeMM', (event, arg) => {
-        $('.message-card__text > div').text(arg);
+        $('.message-card__text > div').html(arg);
         $('.message-card-wrapper').css({'display': 'block'});
-        let mesCard = $('.message-card-wrapper').detach();
+        const mesCard = $('.message-card-wrapper').detach();
         $('body').append(mesCard);
         $(window).scrollTop(0);
     });
@@ -19,10 +19,15 @@ window.addEventListener('load', () => {
     $('head').append('<style type="text/css">::-webkit-scrollbar{width: 0px;}</style>');//スクロールバーを消す
     $(function(){
         if (!$('.message-card-wrapper')) return;
-        let mesCard = $('.message-card-wrapper').detach();
+        const mesCard = $('.message-card-wrapper').detach();
         $('body').empty();
         $('body').removeClass();
         ipcRenderer.send('getGBColor');
         $('body').append(mesCard);
+
+        const text = mesCard.find('.message-card__text');
+        const textDiv = mesCard.find('.message-card__text > div[data-target]').detach();
+        text.empty();
+        text.append(textDiv);
     });
 });
